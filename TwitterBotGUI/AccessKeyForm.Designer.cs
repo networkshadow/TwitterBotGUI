dc54851e678c3c78ca3bc6bbdc35731189645d0c
Logan MacKinnon <loganmackinnon93@gmail.com>
2021-07-06 11:25:25 -0300

    started access page

--- TwitterBotGUI/AccessKeyForm.Designer.cs
+++ TwitterBotGUI/AccessKeyForm.Designer.cs
@@ -28,20 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.txtConsumerKey = new System.Windows.Forms.TextBox();
+            this.txtConsumerSecret = new System.Windows.Forms.TextBox();
+            this.txtAccessToken = new System.Windows.Forms.TextBox();
+            this.txtAccessTokenSecret = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.Location = new System.Drawing.Point(83, 9);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(166, 25);
+            this.lblHeader.TabIndex = 0;
+            this.lblHeader.Text = "Twitter Access";
+            // 
+            // txtConsumerKey
+            // 
+            this.txtConsumerKey.Location = new System.Drawing.Point(12, 96);
+            this.txtConsumerKey.Name = "txtConsumerKey";
+            this.txtConsumerKey.Size = new System.Drawing.Size(309, 20);
+            this.txtConsumerKey.TabIndex = 1;
+            // 
+            // txtConsumerSecret
+            // 
+            this.txtConsumerSecret.Location = new System.Drawing.Point(12, 142);
+            this.txtConsumerSecret.Name = "txtConsumerSecret";
+            this.txtConsumerSecret.Size = new System.Drawing.Size(309, 20);
+            this.txtConsumerSecret.TabIndex = 2;
+            // 
+            // txtAccessToken
+            // 
+            this.txtAccessToken.Location = new System.Drawing.Point(12, 214);
+            this.txtAccessToken.Name = "txtAccessToken";
+            this.txtAccessToken.Size = new System.Drawing.Size(309, 20);
+            this.txtAccessToken.TabIndex = 3;
+            // 
+            // txtAccessTokenSecret
+            // 
+            this.txtAccessTokenSecret.Location = new System.Drawing.Point(12, 260);
+            this.txtAccessTokenSecret.Name = "txtAccessTokenSecret";
+            this.txtAccessTokenSecret.Size = new System.Drawing.Size(309, 20);
+            this.txtAccessTokenSecret.TabIndex = 4;
             // 
             // AccessKeyForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(333, 450);
+            this.Controls.Add(this.txtAccessTokenSecret);
+            this.Controls.Add(this.txtAccessToken);
+            this.Controls.Add(this.txtConsumerSecret);
+            this.Controls.Add(this.txtConsumerKey);
+            this.Controls.Add(this.lblHeader);
             this.Name = "AccessKeyForm";
             this.Text = "Enter Keys & Secrets";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.TextBox txtConsumerKey;
+        private System.Windows.Forms.TextBox txtConsumerSecret;
+        private System.Windows.Forms.TextBox txtAccessToken;
+        private System.Windows.Forms.TextBox txtAccessTokenSecret;
     }
 }
 
