1aa9b181c3a1fb2fe9752d10af604c9f02000a26
Logan MacKinnon <loganmackinnon93@gmail.com>
2021-07-06 11:31:52 -0300

    added graphical interface to form. no backend work yet done.

--- TwitterBotGUI/AccessKeyForm.Designer.cs
+++ TwitterBotGUI/AccessKeyForm.Designer.cs
@@ -33,6 +33,12 @@
             this.txtConsumerSecret = new System.Windows.Forms.TextBox();
             this.txtAccessToken = new System.Windows.Forms.TextBox();
             this.txtAccessTokenSecret = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chkSaveKeys = new System.Windows.Forms.CheckBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblHeader
@@ -73,18 +79,79 @@
             this.txtAccessTokenSecret.Size = new System.Drawing.Size(309, 20);
             this.txtAccessTokenSecret.TabIndex = 4;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "API Key:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "API Key Secret:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Access Token:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 244);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Access Token Secret:";
+            // 
+            // chkSaveKeys
+            // 
+            this.chkSaveKeys.AutoSize = true;
+            this.chkSaveKeys.Location = new System.Drawing.Point(13, 300);
+            this.chkSaveKeys.Name = "chkSaveKeys";
+            this.chkSaveKeys.Size = new System.Drawing.Size(112, 17);
+            this.chkSaveKeys.TabIndex = 9;
+            this.chkSaveKeys.Text = "Save Information?";
+            this.chkSaveKeys.UseVisualStyleBackColor = true;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(12, 341);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 10;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
             // AccessKeyForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(333, 450);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.chkSaveKeys);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtAccessTokenSecret);
             this.Controls.Add(this.txtAccessToken);
             this.Controls.Add(this.txtConsumerSecret);
             this.Controls.Add(this.txtConsumerKey);
             this.Controls.Add(this.lblHeader);
             this.Name = "AccessKeyForm";
-            this.Text = "Enter Keys & Secrets";
+            this.Text = "Enter Keys and Secrets";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -97,6 +164,12 @@
         private System.Windows.Forms.TextBox txtConsumerSecret;
         private System.Windows.Forms.TextBox txtAccessToken;
         private System.Windows.Forms.TextBox txtAccessTokenSecret;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox chkSaveKeys;
+        private System.Windows.Forms.Button btnSubmit;
     }
 }
 
